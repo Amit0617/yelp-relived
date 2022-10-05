9bf17bf20925d2ec87d9e232e53fb0e9fc13e7ea
amit0617 <71893015+Amit0617@users.noreply.github.com>
2022-10-05 13:32:56 +0530

    restaurant by id

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 const express = require('express')
-const db = require('./db')
+// const db = require('./db')
+const { pool } = require('./db');
 
 const morgan = require('morgan');
-const { pool } = require('./db');
+
 const app = express();
 
 //middleware
@@ -14,8 +15,39 @@ app.use(express.json())
 app.get('/api/v1/restaurants', async(req,res) =>{
     let conn;
     try {
-        const result = await db.pool.query("select * from restaurants");
-        res.send(result);
+        const result = await pool.query("select * from restaurants");
+        // res.send(result);
+        // send object with status and length of response
+        res.send({
+            status: "success",
+            count: result.length,
+            data: {
+                restaurants: result
+            }
+        })
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn)
+            return conn.release();
+    }
+})
+
+// Get restaurant on given id
+app.get("/api/v1/restaurants/:id", async(req,res)=>{
+    console.log(req.params.id);
+    let conn;
+    try {
+        const result = await pool.query("select * from restaurants where id = ?", [req.params.id]);
+        // res.send(result);
+        // send object with status and length of response
+        res.send({
+            status: "success",
+            count: result.length,
+            data: {
+                restaurants: result
+            }
+        })
     } catch (err) {
         throw err;
     } finally {
