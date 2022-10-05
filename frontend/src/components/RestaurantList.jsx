b02fbc7e824abcb2ca9b3ceca4d6dc139b974b81
amit0617 <71893015+Amit0617@users.noreply.github.com>
2022-10-05 21:18:02 +0530

    data incorporated in restaurant list

--- frontend/src/components/RestaurantList.jsx
+++ frontend/src/components/RestaurantList.jsx
@@ -11,16 +11,19 @@ import {
     Button
 } from '@chakra-ui/react'
 
-import React, {useEffect} from 'react'
+import React, { useContext, useEffect } from 'react'
+import { RestaurantContext } from '../context/RestaurantContext'
 
-export default function RestaurantList() {
-    
+export default function RestaurantList(props) {
+
+    const { restaurants, setRestaurants } = useContext(RestaurantContext)
+
     useEffect(() => {
-      fetch('http://localhost:3001/api/v1/restaurants/')
-      .then(response => response.json())
-      .then(data => console.log(data))
+        fetch('http://localhost:3001/api/v1/restaurants/')
+            .then(response => response.json())
+            .then(data => setRestaurants(data.data.restaurants))
     }, [])
-    
+
 
     return (
         <TableContainer>
@@ -30,38 +33,25 @@ export default function RestaurantList() {
                     <Tr>
                         <Th>Restaurant</Th>
                         <Th>Location</Th>
-                        <Th isNumeric>Price Range</Th>
+                        <Th>Price Range</Th>
                         <Th>Ratings</Th>
                         <Th>Edit</Th>
                         <Th>Delete</Th>
                     </Tr>
                 </Thead>
                 <Tbody>
-                    <Tr>
-                        <Td>inches</Td>
-                        <Td>millimetres (mm)</Td>
-                        <Td isNumeric>25.4</Td>
-                        <Td>Ratings</Td>
-                        <Td><Button size='md' colorScheme='orange'>Edit</Button></Td>
-                        <Td><Button size='md' colorScheme='red'>Delete</Button></Td>
-
-                    </Tr>
-                    <Tr>
-                        <Td>feet</Td>
-                        <Td>centimetres (cm)</Td>
-                        <Td isNumeric>30.48</Td>
-                        <Td>Ratings</Td>
-                        <Td><Button size='md' colorScheme='orange'>Edit</Button></Td>
-                        <Td><Button size='md' colorScheme='red'>Delete</Button></Td>
-                    </Tr>
-                    <Tr>
-                        <Td>yards</Td>
-                        <Td>metres (m)</Td>
-                        <Td isNumeric>0.91444</Td>
-                        <Td>Ratings</Td>
-                        <Td><Button size='md' colorScheme='orange'>Edit</Button></Td>
-                        <Td><Button size='md' colorScheme='red'>Delete</Button></Td>
-                    </Tr>
+                    {restaurants && restaurants.map((restaurant) => {
+                        return (
+                            <Tr key={restaurant.id} spacing='6'>
+                                <Td>{restaurant.name}</Td>
+                                <Td>{restaurant.location}</Td>
+                                <Td>{'$'.repeat(restaurant.price_range)}</Td>
+                                <Td>Ratings</Td>
+                                <Td><Button size='md' colorScheme='orange'>Edit</Button></Td>
+                                <Td><Button size='md' colorScheme='red'>Delete</Button></Td>
+                            </Tr>
+                        )
+                    })}
                 </Tbody>
                 {/* <Tfoot> */}
                 {/* <Tr> */}
--- frontend/src/context/RestaurantContext.js
+++ frontend/src/context/RestaurantContext.js
@@ -2,7 +2,7 @@ import React, {useState, createContext} from 'react'
 
 export const RestaurantContext = createContext()
 
-export const RestaurantContextProvider = props=> {
+export const RestaurantContextProvider = props => {
   const [restaurants, setRestaurants] = useState([])
   return (
     <RestaurantContext.Provider value={{restaurants, setRestaurants}}>
