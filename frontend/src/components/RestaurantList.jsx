6f51bfcf0fe2cfec79c61046afed08fa14284e2f
amit0617 <71893015+Amit0617@users.noreply.github.com>
2022-10-08 15:39:25 +0530

    Search

--- frontend/src/components/RestaurantList.jsx
+++ frontend/src/components/RestaurantList.jsx
@@ -8,18 +8,25 @@ import {
     TableCaption,
     TableContainer,
     Button,
-    Text
+    Text,
+    Select,
+    Input,
+    InputLeftElement,
+    HStack
 } from '@chakra-ui/react'
 
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useState, useEffect, useCallback } from 'react'
 import { useHistory } from 'react-router-dom'
 import RestaurantFinder from '../api/RestaurantFinder'
 import { RestaurantContext } from '../context/RestaurantContext'
 import StarRating from './StarRating'
-
+import LocationLogo from './LocationLogo'
 export default function RestaurantList(props) {
 
     const { restaurants, setRestaurants } = useContext(RestaurantContext)
+    const [_searchValue, setSearchValue] = useState("")
+    const [_sortingValue, setSortingValue] = useState("2")
+
     let history = useHistory()
 
     useEffect(() => {
@@ -55,6 +62,30 @@ export default function RestaurantList(props) {
         history.push(`/restaurants/${id}/update`)
     }
 
+    const sortCb = useCallback(
+        (restaurantA, restaurantB) => {
+            switch (_sortingValue) {
+                case "2":
+                    return restaurantA[0].localeCompare(restaurantB[0])
+                case "3":
+                    return restaurantB[0].localeCompare(restaurantA[0])
+                case "1":
+                default:
+                    // return getTotalGroupSizes(restaurantB) - getTotalGroupSizes(restaurantA)
+            }
+        },
+        [_sortingValue]
+    )
+
+    const filterCb = useCallback(
+        (oAuthProvider) => {
+            const name = oAuthProvider[0]
+
+            return !_searchValue || name.includes(_searchValue.toLowerCase())
+        },
+        [_searchValue]
+    )
+
     const renderRatings = (restaurant) => {
 
         if (!restaurant.rating_count) {
@@ -74,34 +105,67 @@ export default function RestaurantList(props) {
     }
 
     return (
-        <TableContainer>
-            <Table variant='striped' colorScheme='gray'>
-                <TableCaption>Restaurants available near you</TableCaption>
-                <Thead>
-                    <Tr>
-                        <Th>Restaurant</Th>
-                        <Th>Location</Th>
-                        <Th>Price Range</Th>
-                        <Th>Ratings</Th>
-                        <Th>Edit</Th>
-                        <Th>Delete</Th>
-                    </Tr>
-                </Thead>
-                <Tbody>
-                    {restaurants && restaurants.map((restaurant) => {
-                        return (
-                            <Tr key={restaurant.id} spacing='6'>
-                                <Td cursor='pointer' onClick={() => handleRestaurantClick(restaurant.id)}>{restaurant.name}</Td>
-                                <Td>{restaurant.location}</Td>
-                                <Td>{'$'.repeat(restaurant.price_range)}</Td>
-                                <Td>{renderRatings(restaurant)}</Td>
-                                <Td><Button size='md' colorScheme='orange' onClick={() => handleUpdate(restaurant.id)}>Edit</Button></Td>
-                                <Td><Button size='md' colorScheme='red' onClick={() => handleDelete(restaurant.id)}>Delete</Button></Td>
-                            </Tr>
-                        )
-                    })}
-                </Tbody>
-            </Table>
-        </TableContainer>
+        <>
+            {/* <HStack justify="space-between" mt="80px" mb="10">
+                <InputGroup maxWidth="250px">
+                    <InputLeftElement pointerEvents="none">
+                        <GoSearch color="gray" />
+                    </InputLeftElement>
+                    <Input
+                        colorScheme="primary"
+                        focusBorderColor="primary.500"
+                        placeholder="Search"
+                        value={_searchValue}
+                        onChange={(event) => setSearchValue(event.target.value)}
+                    />
+                </InputGroup> */}
+
+                {/* Sort social networks in alphabetical or inverted order or with most members first */}
+                {/* <Select
+                    value={_sortingValue}
+                    onChange={(event) => setSortingValue(event.target.value)}
+                    maxWidth="250px"
+                    focusBorderColor="primary.500"
+                >
+                    <option value="1">Most members</option>
+                    <option value="2">A-Z</option>
+                    <option value="3">Z-A</option>
+                </Select> */}
+            {/* </HStack> */}
+            
+            <TableContainer>
+                <Table variant='striped' colorScheme='gray'>
+                    <TableCaption>Restaurants available near you</TableCaption>
+                    <Thead>
+                        <Tr>
+                            <Th>Restaurant</Th>
+                            <Th>Location</Th>
+                            <Th>Price Range</Th>
+                            <Th>Ratings</Th>
+                            <Th>Edit</Th>
+                            <Th>Delete</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {restaurants && restaurants
+                            // .sort(sortCB)
+                            // .filter(filterCB)
+                            .map((restaurant) => {
+                                return (
+                                    <Tr key={restaurant.id} spacing='6'>
+                                        <Td cursor='pointer' onClick={() => handleRestaurantClick(restaurant.id)}>{restaurant.name}</Td>
+                                        <Td>{restaurant.location}</Td>
+                                        <Td>{'$'.repeat(restaurant.price_range)}</Td>
+                                        <Td>{renderRatings(restaurant)}</Td>
+                                        <Td><Button size='md' colorScheme='orange' onClick={() => handleUpdate(restaurant.id)}>Edit</Button></Td>
+                                        <Td><Button size='md' colorScheme='red' onClick={() => handleDelete(restaurant.id)}>Delete</Button></Td>
+                                    </Tr>
+                                )
+                            })}
+                    </Tbody>
+                </Table>
+            </TableContainer>
+        </>
+
     )
 }
--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Header from '../components/Header'
 import RestaurantList from '../components/RestaurantList'
 import AddRestaurant from '../components/AddRestaurant'
+import SearchBar from '../components/SearchBar'
 export default function Home() {
   return (
     <div>
         <Header/>
+        <SearchBar />
         <AddRestaurant/>
         <RestaurantList/>
     </div>
