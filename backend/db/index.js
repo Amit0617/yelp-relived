32b3d06379aaf5880a4f19086c5b16d20ed5632b
amit0617 <71893015+Amit0617@users.noreply.github.com>
2022-10-09 18:38:37 +0530

    remote backend

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,24 +1,42 @@
 require('dotenv').config()
 const mariadb = require('mariadb')
+const fs = require('fs');
 
-const pool = mariadb.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    multipleStatements: true,
-    connectionLimit: 5
-});
+const serverCert = fs.readFileSync("db/skysql_chain.pem", "utf8");
 
-pool.getConnection()
-    .then(conn => {
-        console.log("connected ! connection id is " + conn.threadId);
-        conn.release(); //release to pool
-    })
-    .catch(err => {
-        console.log("not connected due to error: " + err);
-    });
+let pool;
+
+// async function callDatabase() {
+
+    try {
+        pool = mariadb.createPool({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            ssl: { ca: serverCert },
+            password: process.env.DB_PASS,
+            port: process.env.DB_PORT,
+            database: process.env.DB_NAME,
+            // multipleStatements: true,
+            // connectionLimit: 5
+        });
+
+        pool.getConnection()
+            .then(conn => {
+                console.log("connected ! connection id is " + conn.threadId);
+                conn.release(); //release to pool
+            })
+            .catch(err => {
+                console.log("not connected due to error: " + err);
+            });
+
+        // await pool.execute('select * from restaurants;')
+    } catch (error) {
+        console.error(error)
+    }
+// }
+
+// callDatabase()
+
 
 //Export it for server
 module.exports = Object.freeze({
